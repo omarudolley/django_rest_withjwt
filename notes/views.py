18cd53ea6a5bb7f5f9afaefef58d81e068bf9e8e
SOLO OMARU DOLLEY <soloomarudolley@pop-os.localdomain>
2020-02-22 17:14:52 +0200

    django_restframework_jwt

---
+++ notes/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self,request,view,obj):
+        return obj.owner == request.user
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Note.objects.filter(owner=user)
+        raise PermissionDenied()
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
